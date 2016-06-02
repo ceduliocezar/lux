a309d1405aeaeb551d83ebd3733b66618e9ad33b
Cedulio Cezar <ceduliocezar@gmail.com>
2016-06-01 22:59:10 -0300

    detail activity

--- lux-android/app/src/main/java/com/ceduliocezar/lux/SplashActivity.java
+++ lux-android/app/src/main/java/com/ceduliocezar/lux/SplashActivity.java
@@ -1,11 +1,13 @@
 package com.ceduliocezar.lux;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -19,6 +21,14 @@ public class SplashActivity extends AppCompatActivity {
 
         GridView gridView = (GridView) findViewById(R.id.movie_grid);
         gridView.setAdapter(new Adapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(SplashActivity.this, MovieDetailActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
@@ -65,5 +75,6 @@ public class SplashActivity extends AppCompatActivity {
 
             return convertView;
         }
+
     }
 }
