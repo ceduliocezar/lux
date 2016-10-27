b5f8f4121059aeca997af802229dd941f443d216
Cedulio Cezar <ceduliocezar@gmail.com>
2016-10-27 18:27:14 -0200

    Fix dependecies and Injection class for prod flavor

---
+++ lux-android/app/src/prod/java/com/ceduliocezar/lux/Injection.java
@@ -0,0 +1,19 @@
+package com.ceduliocezar.lux;
+
+import com.ceduliocezar.lux.data.GenresRepository;
+import com.ceduliocezar.lux.movies.MoviesRepository;
+
+/**
+ * Created by cedulio on 27/10/16.
+ */
+
+public class Injection {
+
+    public static GenresRepository providesGenreRepository(){
+        return null; // TODO: 27/10/16  
+    }
+
+    public static MoviesRepository providesMoviesRepository() {
+        return null; // TODO: 27/10/16
+    }
+}
