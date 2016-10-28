2e1351a5198b92c32a300eb436ab90b20f50e36f
Cedulio Cezar <ceduliocezar@gmail.com>
2016-10-28 17:53:08 -0200

    remove screengrab from circle ci

--- app/src/androidTestMock/java/com/ceduliocezar/lux/home/HomeScreenTest.java
+++ app/src/androidTestMock/java/com/ceduliocezar/lux/home/HomeScreenTest.java
@@ -13,15 +13,9 @@ import com.ceduliocezar.lux.movies.FakeMoviesServiceApiImpl;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import tools.fastlane.screengrab.Screengrab;
-import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
-import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -41,32 +35,20 @@ import static org.hamcrest.Matchers.instanceOf;
 @LargeTest
 public class HomeScreenTest {
 
-    @ClassRule
-    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
-
     @Rule
     public ActivityTestRule<HomeActivity> homeActivityActivityTestRule
             = new ActivityTestRule<>(HomeActivity.class);
 
-    @Before
-    public void setUp(){
-        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
-    }
-
     @Test
     public void clickOnMovie_opensMovieDetailUi() throws Exception {
-        Screengrab.screenshot("beforeSelectmovie");
 
         onView(withText(FakeMoviesServiceApiImpl.FAKE_TITLE + 1)).perform(click());
 
         onView(withId(R.id.movie_overview)).check(matches(isDisplayed()));
-
-        Screengrab.screenshot("afterSelectmovie");
     }
 
     @Test
     public void scrollMovieGrid_loadNextPage() throws Exception {
-        Screengrab.screenshot("beforeLoadNextMovePage");
 
         onData(instanceOf(Movie.class))
                 .inAdapterView(allOf(withId(R.id.movie_grid), isDisplayed()))
@@ -74,8 +56,6 @@ public class HomeScreenTest {
                 .check(matches(isDisplayed()));
 
         onView(withId(R.id.movie_grid)).check(matches(hasNumberOfItems(40)));
-
-        Screengrab.screenshot("afterLoadNextMovePage");
 
     }
 
